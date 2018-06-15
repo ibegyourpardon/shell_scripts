91da42e220a20d4f1764545667d53e45ffb64839
feiandxs <yiziyint@gmail.com>
2018-06-15 16:32:49 +0800

    fixed something?

--- ffmpeg/在macos上安装ffmpeg.sh
+++ ffmpeg/在macos上安装ffmpeg.sh
@@ -1,7 +1,8 @@
+#/bin/bash
 brew install ffmpeg
 
 brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-libass --with-libquvi --with-libvorbis --with-libvpx --with-opus --with-x265
 
-为啥要装两次我也不知道，反正装了就装了。
+#为啥要装两次我也不知道，反正装了就装了。
 
-brew update && brew upgrade ffmpeg% 
+brew update && brew upgrade ffmpeg
